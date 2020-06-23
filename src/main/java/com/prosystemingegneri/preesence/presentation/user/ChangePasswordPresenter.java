941e250b3410576289ff3e5f12a30aca327a7bfa
Davide Mainardi <davide@mainardisoluzioni.com>
2020-06-23 11:34:03 +0200

    Updated copyright.

--- src/main/java/com/prosystemingegneri/preesence/presentation/user/ChangePasswordPresenter.java
+++ src/main/java/com/prosystemingegneri/preesence/presentation/user/ChangePasswordPresenter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2019 Prosystem Ingegneri Affiliati
+ * Copyright (C) 2018-2020 Prosystem Ingegneri Affiliati
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
