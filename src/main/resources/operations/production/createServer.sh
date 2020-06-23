032637f6a0e8330264e7b212dbd9d8ae0e24f9eb
Davide Mainardi <davide@mainardisoluzioni.com>
2020-06-23 12:50:19 +0200

    Deployed in production server (demo prosystem).

--- src/main/resources/operations/production/createServer.sh
+++ src/main/resources/operations/production/createServer.sh
@@ -3,16 +3,16 @@
 source config.shlib; # load the config library functions
 
 #Apache proxy pass and reverse proxy configuration
-ssh root@"$(config_get IP_ADDRESS)" -p "$(config_get EXTERNAL_SSH_PORT)" '\
-a2enmod proxy_http; \
-service apache2 stop; \
-service apache2 start; \
-service apache2 restart; \
-cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/payaraSite.conf; \
-sed -i /etc/apache2/sites-available/payaraSite.conf -e "s/<VirtualHost \*:80>/<VirtualHost *:80>\n\tProxyPass \/ http:\/\/localhost:8080\/\n\tProxyPassReverse \/ http:\/\/localhost:8080\/\n\tProxyPreserveHost on/"; \
-a2dissite 000-default; \
-a2ensite payaraSite; \
-service apache2 restart'
+#ssh root@"$(config_get IP_ADDRESS)" -p "$(config_get EXTERNAL_SSH_PORT)" '\
+#a2enmod proxy_http; \
+#service apache2 stop; \
+#service apache2 start; \
+#service apache2 restart; \
+#cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/payaraSite.conf; \
+#sed -i /etc/apache2/sites-available/payaraSite.conf -e "s/<VirtualHost \*:80>/<VirtualHost *:80>\n\tProxyPass \/ http:\/\/localhost:8080\/\n\tProxyPassReverse \/ http:\/\/localhost:8080\/\n\tProxyPreserveHost on/"; \
+#a2dissite 000-default; \
+#a2ensite payaraSite; \
+#service apache2 restart'
 
 #Database creation
 scp -P "$(config_get EXTERNAL_SSH_PORT)" "$(config_get DB_INITIAL_FILENAME_COMPRESSED)" root@"$(config_get IP_ADDRESS)":/root/
@@ -34,7 +34,7 @@ rm '"$(config_get DB_INITIAL_FILENAME)"''
 #(crontab -l && echo "15 1 * * 0 /root/backupDb.sh >/dev/null 2>&1") | crontab -'
 
 #Copy provided jars into target folder, ready to be copied to production
-mvn -f "$(config_get IDE_WORKSPACE)"/"$(config_get APP_NAME)"/pom.xml -DincludeScope=provided -DexcludeArtifactIds=javax.mail,jakarta.jakartaee-api,activation -DoutputDirectory="$(config_get IDE_WORKSPACE)"/"$(config_get APP_NAME)"/target/libs-temp/ dependency:copy-dependencies
+mvn -f "$(config_get IDE_WORKSPACE)"/"$(config_get APP_NAME)"/pom.xml -DincludeScope=provided -DexcludeArtifactIds=javax.mail,jakarta.jakartaee-api,activation,microprofile -DexcludeTransitive=true -DoutputDirectory="$(config_get IDE_WORKSPACE)"/"$(config_get APP_NAME)"/target/libs-temp/ dependency:copy-dependencies
 #Copy jars into production
 scp -P "$(config_get EXTERNAL_SSH_PORT)" "$(config_get IDE_WORKSPACE)"/"$(config_get APP_NAME)"/target/libs-temp/* root@"$(config_get IP_ADDRESS)":/root/
 
@@ -83,4 +83,7 @@ postgres_'"$(config_get APP_NAME)"'_pool; \
 \
 sh '"$(config_get AS_BIN)"'/asadmin create-jdbc-resource --connectionpoolid postgres_'"$(config_get APP_NAME)"'_pool jdbc/postgres_'"$(config_get APP_NAME)"'; \
 \
+sh '"$(config_get AS_BIN)"'/asadmin restart-domain '"$(config_get AS_DOMAIN_NAME)"'; \
+\
+sh '"$(config_get AS_BIN)"'/asadmin deploy --contextroot "/" ${HOME}/'"$(config_get APP_NAME)"'.war; \
 sh '"$(config_get AS_BIN)"'/asadmin restart-domain '"$(config_get AS_DOMAIN_NAME)"''
